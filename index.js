b22f060cf6ba457c510af5c37c5d6ce841929e85
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2022-06-26 10:16:13 +0600

    products data edited in mongodb

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
-        const productCollection = client.db('warehouse').collection('cycle');
+        const productCollection = client.db('warehouse').collection('cycle2');
         app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
