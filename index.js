23e21fcc27a7a6e3da79dc9aa21d365a411a9881
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2022-06-27 22:53:43 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const ObjectId = require('mongodb').ObjectId;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
@@ -7,16 +8,35 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
+//middleware
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    console.log('inside verifyJWT', authHeader);
+    next();
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuj6pix.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
+        await client.connect();
         const productCollection = client.db('warehouse').collection('bicycle');
+
+        //auth
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ accessToken });
+        })
+
+        // services api
         app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
@@ -35,7 +55,9 @@ async function run() {
             const result = await productCollection.findOne(query);
             res.send(result);
         })
-        app.get('/myProducts', async (req, res) => {
+
+        app.get('/myProducts', verifyJWT, async (req, res) => {
+
             const email = req.query.email;
             console.log(email);
             const query = { email: email };
@@ -44,11 +66,7 @@ async function run() {
             res.send(products);
         })
 
-        // app.get('/myProducts/:email', (req, res) => {
-        //     const email = req.params.email;
-        //     const product = productCollection.find({ email: email }).toArray();
-        //     res.send(product);
-        // })
+
 
         app.delete('/product/:id', async (req, res) => {
             const id = req.params.id;
