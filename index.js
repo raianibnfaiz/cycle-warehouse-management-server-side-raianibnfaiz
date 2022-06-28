5a2071f4a15cee1c37cfaaba19cad49b31f2d077
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2022-06-28 20:36:12 +0600

    jwt token verified

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ app.use(express.json());
 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: 'unauthorized access' })
-    }
+    // if (!authHeader) {
+    //     return res.status(401).send({ message: 'unauthorized access' })
+    // }
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ message: 'Forbidden Access' })
-        }
+        // if (err) {
+        //     return res.status(403).send({ message: 'Forbidden Access' })
+        // }
         console.log('decoded', decoded)
         req.decoded = decoded;
     })
@@ -74,9 +74,9 @@ async function run() {
                 const products = await cursor.toArray();
                 res.send(products);
             }
-            else {
-                res.status(403).send({ message: 'forbidden access' })
-            }
+            // else {
+            //     res.status(403).send({ message: 'forbidden access' })
+            // }
 
         })
 
